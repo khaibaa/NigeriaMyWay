f2b1f53075a2fc2142bceef957c1f0bf5645ef6b
khaibaa <khaibaabagudu@icloud.com>
2024-04-08 22:19:21 +0400

    committed

--- src/components/ui/loading.jsx
+++
@@ -1,9 +0,0 @@
-//A loading spinner component that can be used in any part of the application for loading
-
-export default function LoadingSpinner() {
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-purple-500"></div>
-        </div>
-    );
-}
--- src/lib/utils.js
+++
@@ -1,9 +0,0 @@
-//this works with tailwindcss and clsx to merge classes
-//used to style components
-
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs) {
-  return twMerge(clsx(inputs))
-}
