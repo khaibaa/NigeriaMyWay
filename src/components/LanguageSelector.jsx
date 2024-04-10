311ce97846075d1d1e5e26fd7853271136668c09
Elameen Daiyabu <elameendaiyabu@gmail.com>
2024-04-10 23:09:26 +0100

    edited language selection

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -1,14 +1,19 @@
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
 
-export const LanguageSelector = () =>
-{
-    const [t, i18n] = useTranslation('common');
-    return <div>
-        <span>{t('language-selector.label')} </span>
-        <button onClick={() => i18n.changeLanguage('fr')}>{t('language-selector.languages.fr')}</button>
-        &nbsp;
-        <button onClick={() => i18n.changeLanguage('en')}>{t('language-selector.languages.en')}</button>
-        &nbsp;
-        <button onClick={() => i18n.changeLanguage('es')}>{t('language-selector.languages.es')}</button>
-    </div>
-}
+export const LanguageSelector = () => {
+    const { t, i18n } = useTranslation('common');
+
+    const handleLanguageChange = (e) => {
+        i18n.changeLanguage(e.target.value);
+    };
+
+    return (
+        <div>
+            <select value={i18n.language} onChange={handleLanguageChange}>
+                <option value="en">{t('language-selector.languages.en')}</option>
+                <option value="fr">{t('language-selector.languages.fr')}</option>
+                <option value="es">{t('language-selector.languages.es')}</option>
+            </select>
+        </div>
+    );
+};
